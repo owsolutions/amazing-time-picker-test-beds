ca808068218a55f20a9d7fa6a82f53544a1d3a58
anonymouse <anonymouse>
2018-01-19 05:01:35 +0330

    Install amazing-time-picker

--- ng4-test-bed/src/app/app.module.ts
+++ ng4-test-bed/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AmazingTimePickerModule } from 'amazing-time-picker';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmazingTimePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
